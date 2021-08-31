8f954677c3e17bad942849587dfecb0e8d9879c0
Jonas <jbuus9@gmail.com>
2021-08-31 12:07:47 +0200

    dependency injection added

---
+++ PetRestAPI/Controllers/PetController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PetRestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PetController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"value1", "value2"};
+        }
+
+        [HttpGet("id")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            
+        }
+        
+        
+    }
+}
